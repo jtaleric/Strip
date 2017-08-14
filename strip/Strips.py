4bef7b8d313a1bd63ca52aa933e42ff0bd77edc5
Joe Talerico <jtaleric@redhat.com>
2017-08-14 16:07:08 -0400

    Strip first commit
    Pushing the general structure and testing code

---
+++ strip/Strips.py
@@ -0,0 +1,34 @@
+# This file is part of QUADs.
+#
+# QUADs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# QUADs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QUADs.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+class Strips(object):
+    def __init__(self, data):
+        """
+        Initialize a Hosts object. This is a subset of
+        data required by the Quads object.
+        """
+        self.logger = logging.getLogger("Strip.Strips")
+        self.logger.setLevel(logging.DEBUG)
+        if 'strips' not in data:
+            self.logger.error("data missing required \"Strips\" section.")
+            exit(1)
+
+        self.data = data["strips"]
+
+    # return list of strips
+    def get(self):
+        return sorted(self.data.iterkeys())
